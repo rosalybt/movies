562ca2546c8dc6320436c9d861b617909bf30777
rosalybt <rosalybalbuena.15@gmail.com>
2021-06-09 15:41:04 -0400

    reconfiguring style - themes

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,41 +1,50 @@
 export const theme = {
-    fonts: {
-        families: {
-            title: "Poppins",
-            body: "Poppins",
-            button: "Poppins"
-        },
-    },
+
     colors: {
         primary: "#001529",
         secondary: "#1890ff",
         tertiary: "#fafafa",
         text: "#fff",
-        textLight: "#000",
         border: "#e8e8e8",
         background: "rgba(0, 0, 0, 0.65)",
-
+        // lights
+        primaryLight: "#001529",
+        secondaryLight: "#1890ff",
+        tertiaryLight: "#fafafa",
+        textLight: "#000",
+        backgroundLight: "rgba(0, 0, 0, 0.65)",
     },
     sizes: {
         h1: "30px",
         h2: "25px",
         h3: "20px",
-        p: "10px"
+        p: "10px",
+        cardWidth: "300px",
+        cardHeight: "500px",
 
     },
 
     margin: {
         small: "5px",
-        medium: "10px"
+        medium: "10px",
+        large: "30px",
+        xlarge: "50px"
+    },
+    padding: {
+        small: "5px",
+        medium: "10px",
+        large: "30px",
+        xlarge: "50px"
     },
 
-    breakpoints: {
-        small: "600px",
-        medium: "1000px",
-        large: "1500px"
-    },
     shapes: {
         rounded: "50%",
         corner: "5px"
+    },
+
+    respoonsive: {
+        mobile: "640px",
+        tablet: "1007px",
+        pc: "1008px"
     }
 }
